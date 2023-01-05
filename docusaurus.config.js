916d6a12d893c4f9ea67623e9a1b4edd56cacb0a
Aycan Ogut <aycanonfire@gmail.com>
2023-01-05 18:06:48 +0300

    chore: :wrench: remove edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl:
-            "https://github.com/aycanogut/front-end-resources/blob/main/docs/intro.md",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
