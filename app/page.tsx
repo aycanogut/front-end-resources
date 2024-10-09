a8b30ff1c4e2379917b9e86e073b306c86157e20
aycanogut <aycanonfire@gmail.com>
2024-10-09 20:32:02 +0300

    REMOVE: comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
-// TODO: Find a better way to redirect to dashboard
 export default function HomePage() {
-  redirect('/resources')
+  redirect('/resources');
 }
